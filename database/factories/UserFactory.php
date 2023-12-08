56cfd374ae7cb4d0539bc3d85f134fd178df0831
celaraze <celaraze@qq.com>
2023-12-09 01:45:58 +0800

    ⌛️ 修复测试类失败的问题

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,19 +21,8 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => bcrypt('password'),
             'remember_token' => Str::random(10),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
